1854bcbae07663ca545baee5d446619a5672623b
zhangsan <111@qq.com>
2020-09-29 16:13:05 +0800

    1

---
+++ springcloud_demo/config_client/src/main/java/com/cicro/config_client/TestConfig.java
@@ -0,0 +1,19 @@
+package com.cicro.config_client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestConfig {
+
+    @Value("${active}")
+    String active;
+
+    @GetMapping("/test")
+    public String test() {
+        return active;
+    }
+}
